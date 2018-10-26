33934af9144d11ffbf2300f8caafb70f2757d29c
Gen <archiherewego@gmail.com>
2018-10-26 16:44:40 +0900

    solution of Jewels and stones

--- src/main/kotlin/leatcode/JewelsAndStones.kt
+++ src/main/kotlin/leatcode/JewelsAndStones.kt
@@ -13,15 +13,11 @@ class JewelsAndStones {
     
     
     // 次にHashMapを使ってSの文字種類別に出現数をカウント、Jをキーとしてカウント数を足し合わせる方法をとった
-    // ただし速度は改善されない
+    // ただし速度は改善されない（というかどうやっても大差ないみたい）
     // 要素数分ループしているのは変わらないからだろうか
+    // leatcode上での実行時間を基準にすると、実行タイミングによって結果が変わる
+    // そもそもやり方によって大差が生まれない問題であれば誤差なので当てにならなそう（Kotlinは特に）
     fun numJewelsInStones(J: String, S: String): Int {
-        var count = 0
-        val map = hashMapOf<Char, Int>()
-        S.forEach {
-            map[it] = map[it]?.plus(1) ?: 1
-        }
-        J.forEach { count += map[it] ?: 0 }
-        return count
+        return S.filter {J.contains(it)}.length
     }
 }
