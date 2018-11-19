92d54aabb940991699f117460d3b905c7787c82b
Gen <archiherewego@gmail.com>
2018-11-19 18:19:24 +0900

    Note added: Manacher's Algorithm
    こういうアルゴリズムがあって、これを使えばさらに効率化できるということらしい
    が、コスパの観点からこういうアルゴリズムがあるということだけメモしておくに留める

--- src/main/kotlin/leatcode/medium/PalindromicSubstrings.kt
+++ src/main/kotlin/leatcode/medium/PalindromicSubstrings.kt
@@ -6,6 +6,15 @@ package leatcode.medium
  * 与えられた文字列のうちに回文となる部分文字列がいくつあるかを返す
  * 部分文字列が同じ文字列であっても、indexが異なれば別物としてカウントする
  * (例えばaaaという文字列のindex0:1のaaとindex1:2のaaは異なるものとしてカウントする)
+ *
+ * 解法をみてもさっぱり理解できないので、またどこかで出くわしたら頑張ってみる
+ * TODO: Manacher's Algorithm
+ * http://snuke.hatenablog.com/entry/2014/12/02/235837
+ * https://qiita.com/Shawna/items/17ed870cd4c8c0f1f478
+ *
+ * Qiitaの解説がわかりやすいか
+ * 文字列の各中心点における最長の回文を見つければいいという考え方っぽい？
+ * (回文は両端を同じ長さだけ削っても回文なので、最も長い回文が見つかればその回文の長さからいくつ回文がとれるかがわかる)
  */
 class PalindromicSubstrings {
     
